914c83232e09e47fe5d22cc4dd06ba90372a3973
Shivam Gangwar <shivamgangwar@pool-100-0-230-218.bstnma.east.verizon.net>
2026-01-30 13:04:27 +0530

    nested_ctes

--- Advance SQL/CTE/recursive_cte.sql
+++ Advance SQL/CTE/recursive_cte.sql
@@ -8,7 +8,6 @@ union all
     from CTE_name
     where terminating_condition
 )
-*/
 
 -- Generate a sequence of numbers from 1 to 20
 WITH CTE_generate AS(
@@ -17,8 +16,33 @@ WITH CTE_generate AS(
 UNION ALL
     SELECT MyNumber+1
     FROM CTE_generate
-    WHERE MyNumber <>20
+    WHERE MyNumber <1000
 )
 SELECT *
 FROM CTE_generate
+OPTION(MAXRECURSION 5000)
+*/
 
+-- Show employee hierarchy by displaying each level within the organisation
+WITH CTE_hierarchy AS(
+-- Anchor query
+SELECT 
+EmployeeID,
+FirstName,
+ManagerID,
+1 AS level
+FROM Sales.Employees
+WHERE ManagerID IS NULL
+-- Recursive query
+UNION ALL
+SELECT e.EmployeeID,
+e.FirstName,
+e.ManagerID,
+level+1
+FROM Sales.Employees AS e
+INNER JOIN CTE_hierarchy ceh
+ON e.ManagerID = ceh.EmployeeID
+)
+
+SELECT *
+FROM CTE_hierarchy
