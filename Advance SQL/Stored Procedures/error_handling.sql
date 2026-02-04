27b1b8b7fbe0e94e6fc4c5b1bb54f73fd4f4b162
Shivam Gangwar <shivamgangwar@pool-100-0-242-198.bstnma.fios.verizon.net>
2026-02-04 16:52:11 +0530

    procedures

---
+++ Advance SQL/Stored Procedures/error_handling.sql
@@ -0,0 +1,52 @@
+alter procedure GetCustomerSummary @Country NVARCHAR(50)= 'USA' as
+    begin 
+    DECLARE @TotalCustomers INT, @AvgScore FLOAT;
+-- Preparing & clean up dataselect 1 from Sales.Customers where Score is null and @Country = 'USA'
+IF EXISTS (select 1 from Sales.Customers where Score is null and Country = @Country)
+BEGIN
+BEGIN TRY
+
+print 'Updating nulls into 0';
+update Sales.Customers
+set Score = 0
+where Score is null and Country = @Country
+END
+
+ELSE
+BEGIN
+print 'No null scores are found'
+END;
+
+-- Generating report
+
+    SELECT
+    @TotalCustomers = COUNT(*),
+    @AvgScore = SUM(Score)
+    FROM Sales.Customers
+    where Country = @Country;
+
+    PRINT 'Total customers from '+ @Country+ ':'+ CAST(@TotalCustomers as NVARCHAR);
+    PRINT 'Average score from'+ @Country+ ':'+ CAST(@AvgScore as NVARCHAR);
+
+    SELECT
+    COUNT(OrderID) as TotalOrders,
+    SUM(Sales) as TotalSales,
+    1/0
+    FROM Sales.Orders o 
+    JOIN Sales.Customers c 
+    ON c.CustomerID = o.CustomerID
+    WHERE c.Country = @Country
+
+    END TRY
+    BEGIN CATCH
+    PRINT 'An error occured.';
+    PRINT ('Error message: ' + error_message());
+    PRINT ('Error number: ' + CAST(error_number() AS NVARCHAR));
+    PRINT ('Error line: ' + CAST(error_line() AS NVARCHAR));
+    PRINT ('Error procedure: ' + error_procedure());
+
+    END CATCH
+    END
+    GO
+
+    EXEC GetCustomerSummary;
--- Advance SQL/Stored Procedures/if_else.sql
+++ Advance SQL/Stored Procedures/if_else.sql
@@ -1,6 +1,21 @@
-create procedure GetCustomerSummary @Country NVARCHAR(50)= 'USA' as
+alter procedure GetCustomerSummary @Country NVARCHAR(50)= 'USA' as
     begin 
     DECLARE @TotalCustomers INT, @AvgScore FLOAT;
+-- Preparing & clean up dataselect 1 from Sales.Customers where Score is null and @Country = 'USA'
+IF EXISTS (select 1 from Sales.Customers where Score is null and Country = @Country)
+BEGIN
+print 'Updating nulls into 0';
+update Sales.Customers
+set Score = 0
+where Score is null and Country = @Country
+END
+
+ELSE
+BEGIN
+print 'No null scores are found';
+END;
+
+-- Generating report
 
     SELECT
     @TotalCustomers = COUNT(*),
@@ -13,7 +28,8 @@ create procedure GetCustomerSummary @Country NVARCHAR(50)= 'USA' as
 
     SELECT
     COUNT(OrderID) as TotalOrders,
-    SUM(Sales) as TotalSales
+    SUM(Sales) as TotalSales,
+    1/0
     FROM Sales.Orders o 
     JOIN Sales.Customers c 
     ON c.CustomerID = o.CustomerID
@@ -24,4 +40,4 @@ create procedure GetCustomerSummary @Country NVARCHAR(50)= 'USA' as
     EXEC GetCustomerSummary;
     EXEC GetCustomerSummary @Country='Germany';
 
-
+select * from Sales.Customers
